7043708a268f4a144c7789573eb758639aa02750
Labtory-82 <chnny9@gmail.com>
2025-08-27 11:16:49 +0900

    asdfdsf

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,17 +2,21 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("목록")
-    public void index () {
-        System.out.println("index() 메서드 호출됨!");
+    @ResponseBody
+    public String index () {
+        return "sbb";
     }
 
+    @ResponseBody
     @GetMapping("등록")
-    public void hello () {
-        System.out.println("hello() 메서드 호출됨!");
+    public String hello () {
+        return "hello";
     }
+
 }
